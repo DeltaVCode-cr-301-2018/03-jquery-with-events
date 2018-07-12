36f67b0c0b56c99f1a50bf65317c625e0372f107
Ethan Titler <Titler.ethan.chs@gmail.com>
2018-07-12 10:00:03 -0500

    fixed the tab effect on outside links

--- Ethan-Taylor/scripts/articleView.js
+++ Ethan-Taylor/scripts/articleView.js
@@ -77,7 +77,7 @@ articleView.handleMainNav = function() {
   // TODO: Add an event handler to nav elements that will power the Tabs feature.
   // Clicking any .tab element should hide all the .tab-content sections, and then reveal the single .tab-content section that is associated with the clicked .tab element.
   // So: You need to dynamically build a selector string with the correct ID, based on the data available to you on the .tab element that was clicked.
-  $('nav li').on('click',function(){
+  $('nav li').on('click','.tab',function(){
     $('.tab-content').hide();
     let $activeTab=$(this).data('content');
     console.log($(this).data('content'));
